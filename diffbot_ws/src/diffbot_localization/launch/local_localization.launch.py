80e379ab0483edf8a0f7d06152c75ad117af15d8
Mario Gomez <m.gomez03016@gmail.com>
2025-05-01 22:17:29 +0200

    local localization launch

--- diffbot_ws/src/diffbot_localization/launch/local_localization.launch.py
+++ diffbot_ws/src/diffbot_localization/launch/local_localization.launch.py
@@ -15,7 +15,19 @@ def generate_launch_description():
         default_value="False"
     )
 
+    is_real_arg = DeclareLaunchArgument(
+        "is_real",
+        default_value="False"
+    )
+
+    imu_topic_arg = DeclareLaunchArgument(
+        "imu_topic",
+        default_value="imu/out"
+    )
+
     use_python = LaunchConfiguration("use_python")
+    is_real = LaunchConfiguration("is_real")
+    imu_topic = LaunchConfiguration("imu_topic")
 
     static_transform_publisher = Node(
         package="tf2_ros",
@@ -24,33 +36,44 @@ def generate_launch_description():
                    "--frame-id", "base_link_ekf", "--child-frame-id", "imu_link_ekf"]
     )
 
-    robot_localization = Node(
+    robot_localization_sim = Node(
         package="robot_localization",
         executable="ekf_node",
         name="ekf_filter_node",
         output="screen",
-        parameters=[os.path.join(get_package_share_directory("diffbot_localization"),"config", "ekf.yaml")]
+        parameters=[os.path.join(get_package_share_directory("diffbot_localization"),"config", "ekf.yaml")],
+        condition=UnlessCondition(is_real)
+    )
+
+    robot_localization_real = Node(
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[os.path.join(get_package_share_directory("diffbot_localization"),"config", "ekf_real.yaml")],
+        condition=IfCondition(is_real)
     )
 
     imu_republisher_py = Node(
         package="diffbot_localization",
         executable="imu_republisher_py",
-        parameters=[{"imu_topic":"imu/out"}],
+        parameters=[{"imu_topic": imu_topic}],
         condition=IfCondition(use_python)
     )
 
     imu_republisher_cpp = Node(
         package="diffbot_localization",
         executable="imu_republisher",
-        parameters=[{"imu_topic":"imu/out"}],
+        parameters=[{"imu_topic": imu_topic}],
         condition=UnlessCondition(use_python)
     )
 
-
     return LaunchDescription([
         use_python_arg,
+        is_real_arg,
         static_transform_publisher,
-        robot_localization,
+        robot_localization_sim,
+        robot_localization_real,
         imu_republisher_py,
         imu_republisher_cpp
     ])
