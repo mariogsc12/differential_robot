b6e9cbd41e553140663f487acf6a6aba10cd5dca
Mario Gomez <m.gomez03016@gmail.com>
2025-05-02 19:30:52 +0200

    modify the ros killer

--- utils/kill_ros2.sh
+++ utils/kill_ros2.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 # ros2 process to kill
-processes=("ros2", "_ros2_daemon", "ros2-daemon" ,"rviz2", "gzserver", "gzclient", "robot_state_publisher",
-	"controller_server", "lifecycle_manager", "ros2_daemon", "ros2_launch", "ros2_control_node",
-	"witmotion_ros")
+processes=(
+  "ros2"
+  "from ros2cli.daemon.daemonize import main"
+  "rviz2"
+  "gzserver"
+  "gzclient"
+  "robot_state_publisher"
+  "controller_server"
+  "lifecycle_manager"
+  "ros2_daemon"
+  "ros2-daemon"
+  "ros2_launch"
+  "ros2_control_node"
+  "witmotion_ros"
+  "diffbot_controller"
+  "diffbot_bringup"
+)
+
 
 for process in "${processes[@]}"; do
 	pkill -9 -f $process
